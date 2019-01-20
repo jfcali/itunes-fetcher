34860b98154bb99c09e9ebc7e6b6192975c2fa8b
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-20 21:03:06 +0100

    Add grid layout and sticky logic

--- src/components/Catalogue/Catalogue.js
+++ src/components/Catalogue/Catalogue.js
@@ -5,7 +5,18 @@ import Album from './Album/Album';
 
 const Catalogue = props => {
   return (
-    <div className={styles.Catalogue}>
+    <div
+      className={styles.Catalogue}
+      style={{
+        marginTop: props.sticky ? '100px' : null
+      }}
+    >
+      {props.albums.map(a => {
+        const { artist, album, imageUrl, id } = a;
+        return (
+          <Album key={id} artist={artist} album={album} imageUrl={imageUrl} />
+        );
+      })}
       <Album />
     </div>
   );
