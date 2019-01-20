0ff0f1f72021e92eee2357126150c770e89b09f5
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-20 21:03:48 +0100

    Add initial album detail layout
    Add initial album detail layout

--- src/components/Catalogue/Album/Album.js
+++ src/components/Catalogue/Album/Album.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import styles from './Album.module.css';
+
+const ellipsis = ({ text, tail = '...', length = 100 }) => {
+  console.log(text);
+  if (!text) return null;
+  return text.length <= length
+    ? text
+    : [...text].splice(length).join('') + tail;
+};
 
 const album = props => {
-  return <p>here lies an album</p>;
+  return (
+    <div className={styles.Album}>
+      <img
+        src={props.imageUrl}
+        className={styles.Cover}
+        alt={props.album}
+        longdesc={props.album}
+      />
+      <div>
+        <h1 className={styles.AlbumName}>{props.album}</h1>
+        <p className={styles.Artist}>{ellipsis({ text: props.artist })}</p>
+      </div>
+    </div>
+  );
 };
 
 export default album;
