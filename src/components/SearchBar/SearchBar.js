80f1fe3ee9b245f8cdb1713bbf8dc079e68cc0fc
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-20 21:02:40 +0100

    Add sticky and query text logic

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './SearchBar.module.css';
+
+import { getAlbums } from '../../store/actions/actions';
+import { connect } from 'react-redux';
 
 import Field from '../UI/Field/Field';
 import Clear from '../UI/Clear/Clear';
 import Submit from '../UI/Submit/Submit';
 
-const searchBar = props => {
-  return (
-    <header className={styles.SearchBar}>
-      <Field type="text" />
-      <Clear disabled>x</Clear>
-      <Submit>SEARCH</Submit>
-    </header>
-  );
-};
+class SearchBar extends Component {
+  state = {
+    query: ''
+  };
 
-export default searchBar;
+  updateText = e => {
+    this.setState({
+      query: e.target.value
+    });
+  };
+
+  clearText = () => {
+    this.setState({
+      query: ''
+    });
+  };
+
+  handleEnter = e => {
+    if (e.key === 'Enter') {
+      this.props.fetchAlbums({ query: this.state.query });
+    }
+  };
+
+  render() {
+    const classes = [styles.SearchBar];
+    if (this.props.sticky) classes.push(styles.Sticky);
+    return (
+      <header className={classes.join(' ')}>
+        <Field
+          type="text"
+          value={this.state.query}
+          onChange={this.updateText.bind(this)}
+          onKeyPress={this.handleEnter}
+          disabled={this.props.loading}
+        />
+        <Clear
+          clickhandler={this.clearText.bind(this)}
+          disabled={
+            this.props.loading || (this.state.query.length > 0 ? false : true)
+          }
+        >
+          x
+        </Clear>
+        <Submit
+          clicked={() => {
+            return this.props.fetchAlbums({ query: this.state.query });
+          }}
+          disabled={
+            this.props.loading || (this.state.query.length > 0 ? false : true)
+          }
+        >
+          SEARCH
+        </Submit>
+      </header>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAlbums: ({ query }) => {
+      return dispatch(getAlbums({ query }));
+    }
+  };
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);
