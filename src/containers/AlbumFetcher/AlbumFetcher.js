91c2f3c0033abfbf9b0e499fa21f3e2f97040632
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-21 23:57:40 +0100

    Add message logic

--- src/containers/AlbumFetcher/AlbumFetcher.js
+++ src/containers/AlbumFetcher/AlbumFetcher.js
@@ -6,6 +6,7 @@ import { goToNavigation } from '../../store/actions/actions';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Catalogue from '../../components/Catalogue/Catalogue';
 import Navigation from '../../components/Navigation/Navigation';
+import Message from '../../components/Message/Message';
 
 class AlbumFetcher extends Component {
   state = {
@@ -29,34 +30,55 @@ class AlbumFetcher extends Component {
 
   render() {
     return (
-      <div className={styles.AlbumFetcher}>
+      <div
+        className={
+          this.state.sticky
+            ? [styles.AlbumFetcher, styles.Sticky].join(' ')
+            : styles.AlbumFetcher
+        }
+      >
         <SearchBar sticky={this.state.sticky} loading={this.props.loading} />
-        <Catalogue
-          sticky={this.state.sticky}
-          loading={this.props.loading}
-          albums={this.props.albums}
-          currentPage={this.props.currentPage}
-          albumsPerPage={this.props.albumsPerPage}
-        />
-        <Navigation
-          currentPage={this.props.currentPage}
-          totalPages={this.props.totalPages}
-          goTo={this.props.goTo}
-        />
+        {this.props.albums && this.props.albums.length ? (
+          <Catalogue
+            sticky={this.state.sticky}
+            loading={this.props.loading}
+            albums={this.props.albums}
+            currentPage={this.props.currentPage}
+            albumsPerPage={this.props.albumsPerPage}
+            error={this.props.error}
+          />
+        ) : (
+          <Message loading={this.props.loading} error={this.props.error} />
+        )}
+        {this.props.albums && this.props.albums.length ? (
+          <Navigation
+            currentPage={this.props.currentPage}
+            totalPages={this.props.totalPages}
+            goTo={this.props.goTo}
+          />
+        ) : null}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { loading, albums, currentPage, albumsPerPage, totalPages } = state;
+  const {
+    loading,
+    albums,
+    currentPage,
+    albumsPerPage,
+    totalPages,
+    error
+  } = state;
 
   return {
     loading,
     albums: albums,
     currentPage,
     totalPages,
-    albumsPerPage
+    albumsPerPage,
+    error
   };
 };
 
