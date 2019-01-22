6704c96e337856d7354fde0aecc8b7ed2b968693
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-22 23:41:59 +0100

    Update navigation layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import styles from './Navigation.module.css';
 
+import Button from '../UI/Button/Button';
+
 const navigation = props => {
+  const additionalStyles = {
+    padding: '10px 20px',
+    boxSizing: 'border-box',
+    margin: '0px 20px'
+  };
   return (
     <div className={styles.Navigation}>
-      <button onClick={() => props.goTo({ index: props.currentPage - 1 })}>
+      <Button
+        style={additionalStyles}
+        onClick={() => props.goTo({ index: props.currentPage - 1 })}
+      >
         PREV
-      </button>
+      </Button>
       {`${props.currentPage} / ${props.totalPages}`}
-      <button onClick={() => props.goTo({ index: props.currentPage + 1 })}>
+      <Button
+        style={additionalStyles}
+        onClick={() => props.goTo({ index: props.currentPage + 1 })}
+      >
         NEXT
-      </button>
+      </Button>
     </div>
   );
 };
