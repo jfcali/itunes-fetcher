8d780f03c89ec18cf962b44bde08ca2723204c44
Juan Gómez <juanfelipegomez.c@gmail.com>
2019-01-25 00:35:05 +0100

    Add Redux Dev Tools setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import fetchReducer from './store/reducers/FetchReducer';
 
-const store = createStore(fetchReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  fetchReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
